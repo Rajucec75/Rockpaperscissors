bb27e3577c3023338eda7752fa0675b976ea3af5
raj vittal <39086276+Rajucec75@users.noreply.github.com>
2023-07-21 10:52:37 +1000

    Update comments to be more clear

--- script.js
+++ script.js
@@ -49,40 +49,83 @@ function capturePlayerchoice(){  // this function prompts user to provide valid 
 
 function determineWinner(computerSelection,lowercaseChoice){   // this function determines the winner by comparing the computer selection with that of player selections
 
-    let declareWinner;
+    let declareWinner;                                  // holds value to winner and returns back to calling module
+    
 
-    if (computerSelection == "rock" ) {                 // Determine the winner when computer selection is rock with that of player selection
+    if (computerSelection == "rock" ) {                 // Determine the winner when computer selection is rock with that of player selection and increment win count
         if (lowercaseChoice == "paper") {
             declareWinner = "You Win! Paper beats Rock";
+            playerWincount++;
             
         }
             else if (lowercaseChoice == "scissor")  {       
             declareWinner = "Computer wins, Rock beats scissor";
+            computerWincount++;
             
-        } else declareWinner = "It's a tie";
+        } else {
+            declareWinner = "It's a tie";
+            /*playerWincount++;
+            computerWincount++;*/
+        }
 
-    } else if (computerSelection == "paper") {             // Determine the winner when computer selection is paper with that of player selection
-        if (lowercaseChoice == "scissor") 
+    } else if (computerSelection == "paper") {             // Determine the winner when computer selection is paper with that of player selection and increment win count
+        if (lowercaseChoice == "scissor") {
             declareWinner = "You Win! Scissor beats Paper";
-            else if (lowercaseChoice == "rock") 
+            playerWincount++;
+        }else if (lowercaseChoice == "rock") {
             declareWinner = "Computer wins, Paper beats Rock";
-            else declareWinner = "It's a tie";
+            computerWincount++;
+        }else {
+            declareWinner = "It's a tie";
+            /*playerWincount++;
+            computerWincount++;*/
+        }
+            
+            
 
 
-    } else if (computerSelection == "scissor") {           // Determine the winner when computer selection is scissor with that of player selection
-        if (lowercaseChoice == "rock") 
+    } else if (computerSelection == "scissor") {           // Determine the winner when computer selection is scissor with that of player selection and increment win count
+        if (lowercaseChoice == "rock") {
             declareWinner = "You Win! Rock beats Scissor";
-            else if (lowercaseChoice == "paper") 
+            playerWincount++;
+        }else if (lowercaseChoice == "paper") {
             declareWinner = "Computer wins, Scissor beats paper";
-            else declareWinner = "It's a tie";
+            computerWincount++;
+        }else {
+            declareWinner = "It's a tie";
+            /*playerWincount++;
+            computerWincount++;*/
+        }
 
     } 
 
     return declareWinner;    //Return the winner to calling module 
 }
 
-let computerSelection = generateComputerchoice();  //Generate computer selection
-console.log(computerSelection);
-let lowercaseChoice = capturePlayerchoice();       //Capture Player selection
-console.log(lowercaseChoice);
-console.log(determineWinner(computerSelection, lowercaseChoice));  //Determine the winner
+function game(){
+    let computerSelection = generateComputerchoice();  //Generate computer selection
+    console.log(computerSelection);
+    let lowercaseChoice = capturePlayerchoice();       //Capture Player selection
+    console.log(lowercaseChoice);
+    
+    console.log(determineWinner(computerSelection, lowercaseChoice));  //Determine the winner
+}
+
+let playerWincount=0;                 // variable to hold player win counts
+let computerWincount=0;               // variable to hold Computer win counts
+for (i=0;i<5;i++){                    // Iterate the game 5 times 
+    game();
+}
+
+
+function declareWinner (){
+    if (playerWincount > computerWincount){            // most of wins of the 5 , will be the winner of the game!
+        console.log("Player wins the game");
+    } else if(computerWincount>playerWincount){
+        console.log("Computer wins the game");
+    } else console.log("Its's a tie");
+    
+}
+
+console.log(playerWincount);
+console.log(computerWincount);
